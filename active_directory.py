2f7fa6a3460880077b28ef60b93f3f02864ea1d8
Philip Bove <pgbson@gmail.com>
2017-12-31 11:33:13 -0500

    lookups are working

--- active_directory.py
+++ active_directory.py
@@ -8,6 +8,7 @@ import json
 import os
 parser = argparse.ArgumentParser(description='Dynamic inventory script for Active Directory')
 parser.add_argument('-l','--list',help='list inventory',required=True,action='store_true')
+parser.add_argument('-d','--debug',help='Enable error message dumping',action='store_true')
 args = parser.parse_args()
 
 def main():
@@ -22,10 +23,12 @@ def main():
 	if config['config']['inventory'] == 'groups':
 		ad_inv_by_security_group(config, inv_list)
 	elif config['config']['inventory'] == 'ou':
-		ad_inv_by_security_ou(config, inv_list)
+		ad_inv_by_ou(config, inv_list)
 	else:
 		print('The only valid options for "inventory" are groups/ou',file=sys.stderr)
 		sys.exit(1)
+	print(json.dumps(inv_list, indent=4))
+	sys.exit(0)
 
 #def group_lookup(computers, inv_list):
 
@@ -36,17 +39,35 @@ def ad_connection(config):
 			password=config['config']['password'], authentication=ldap3.NTLM, raise_exceptions=True, auto_referrals=False)
 		connection.bind()
 		return connection
-	except KeyError:
+	except KeyError as error:
 		print('You are missing values from active_directory.ini', file=sys.stderr)
+		if args.debug:
+			print(error, file=sys.stderr)
 		sys.exit(1)
-	except ldap3.core.exceptions.LDAPOperationResult:
+	except ldap3.core.exceptions.LDAPOperationResult as error:
 		print('Could not bind!',file=sys.stderr)
+		if args.debug:
+			print(error, file=sys.stderr)
 		sys.exit(1)
-	except ValueError:
-		print('"use_ssl" must be True of False and "port" must be an int', file=sys.stderr)
+	except ValueError as error:
+		print('"port" must be an int', file=sys.stderr)
+		if args.debug:
+			print(error, file=sys.stderr)
 		sys.exit(1)	
 
-#Currently does not lookup past one level
+def add_to_list(group_name, computer, inv_list):
+	if group_name not in inv_list and len(computer['attributes']['dnshostname']) > 1:
+		inv_list[group_name] = [computer['attributes']['dnshostname']]
+	elif group_name not in inv_list:
+		inv_list[group_name] = [computer['attributes']['cn'].lower()]
+	elif group_name in inv_list and len(computer['attributes']['dnshostname']) > 1:
+		if computer['attributes']['dnshostname'] not in inv_list[group_name]:
+			inv_list[group_name].append(computer['attributes']['dnshostname'])
+	else:
+		if computer['attributes']['cn'] not in inv_list[group_name]:
+			inv_list[group_name].append(computer['attributes']['cn'].lower())
+
+#Currently does not lookup past one level of group nesting
 def ad_inv_by_security_group(config, inv_list):
 	connection = ad_connection(config)
 	try:
@@ -56,32 +77,41 @@ def ad_inv_by_security_group(config, inv_list):
 			for computer in connection.response:
 				#print(computer)
 				if 'attributes' in computer:
-					for groups in computer['attributes']['memberof']:
-						if len(groups) > 0:
-							group_name = groups.split(',')[0].split('=')[1]
-							if group_name not in inv_list and len(computer['attributes']['dnshostname']) > 1:
-								inv_list[group_name] = [computer['attributes']['dnshostname']]
-							elif group_name not in inv_list:
-								inv_list[group_name] = [computer['attributes']['cn'].lower()]
-							elif group_name in inv_list and len(computer['attributes']['dnshostname']) > 1:
-								inv_list[group_name].append(computer['attributes']['dnshostname'])
-							else:
-								inv_list[group_name].append(computer['attributes']['cn'].lower())
-		print(inv_list)
-
-		sys.exit()
+					if len(computer['attributes']['memberof']) > 0:
+						for groups in computer['attributes']['memberof']:
+							add_to_list(groups.split(',')[0].split('=')[1],computer, inv_list)
+					#Hard coded to include domain controllers (The have primarygroup but that is not in the ldap spec
+					# and not supported in ldap3)
+					if computer['dn'].split(',')[1].split('=')[1] == 'Domain Controllers':
+						add_to_list('Domain Controllers',computer,inv_list)
 
 	except ldap3.core.exceptions.LDAPOperationResult as error:
-		print(error)
 		print('Something went wrong while searching, are your OUs correct?',file=sys.stderr)
+		if args.debug:
+			print(error, file=sys.stderr)
 		sys.exit(1)	
-	except KeyError as error:
-		print(error)
-		print('You are missing values from active_directory.ini', file=sys.stderr)
-		sys.exit(1)
 
-def ad_inv_by_ou(config):
+def ad_inv_by_ou(config, inv_list):
 	connection = ad_connection(config)
+	try:
+		for ou in config['config']['ous'].split(':'):
+			connection.search(search_filter='(objectclass=computer)',search_base=ou,
+				attributes=['dnshostname','cn'])
+			for computer in connection.response:
+				if 'attributes' in computer:
+					groups = computer['dn'].split(',')
+					#get rid of object itself
+					del groups[0]
+					for ou in groups:
+						if 'cn' in ou.lower() or 'ou' in ou.lower():
+							add_to_list(ou.split('=')[1],computer, inv_list)
+			
+	except ldap3.core.exceptions.LDAPOperationResult as error:
+		print('Something went wrong while searching, are your OUs correct?',file=sys.stderr)
+		if args.debug:
+			print(error, file=sys.stderr)
+		sys.exit(1)	
+
 
 if __name__ == '__main__':
 	main()
